8919f07aa71a964a633323c9c3658e5cd7842cbc
Artiom_Sokolov <sokolova-net@mail.ru>
2022-07-19 21:29:27 +0300

    publish

--- gulpfile.js
+++ gulpfile.js
@@ -339,6 +339,6 @@ const gulp = require('gulp');
 const ghPages = require('gulp-gh-pages');
 
 gulp.task('deploy', function() {
-  return gulp.src('./app/**/*')
+  return gulp.src('./build/**/*')
     .pipe(ghPages());
 });
